22b429608d1cf8ead194f66e0ed9b336a0faffa8
Robert Porsch <rmporsch@gmail.com>
2017-06-22 15:40:04 +0800

    moved makefile example to new rep, finished rmarkdown example

--- part_2/example/load_packages.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-####################################
-#  Installs Packages If necessary  #
-####################################
-
-list.of.packages <- c("tidyverse", "rmarkdown")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
